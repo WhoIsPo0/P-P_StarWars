cec8ef0db0064f9b76ab38e8b928bb52ec9bd11e
Pouya Tavakoli <69327223+pouyatavakoli@users.noreply.github.com>
2023-12-26 11:54:58 +0330

    fixed chart dimention 10*10 and added intitialize game function

--- StarWars.cpp
+++ StarWars.cpp
@@ -1,27 +1,38 @@
 #include <iostream>
 using namespace std;
 
+void initializeGame ();
 void Chart();
 
 int main(){
-    Chart();
+    
+    initializeGame();
+    
     return 0;
 }
 
+// define chart function
 void Chart(){
 
     for(int i = 0 ; i <= 10 ; i++){
-        for(int j = 0 ; j <= 10 ; j++)
+        for(int j = 0 ; j <= 9 ; j++)
             cout << " ---";
 
         cout << endl;
 
         if( i <= 9) {
-            for (int k = 0; k <= 11; k++)
+            for (int k = 0; k <= 10; k++)
                 cout << "|   ";
 
             cout << endl;
         }
 
     }
+}
+
+// define initializeGame function
+
+void initializeGame(){
+
+    Chart () ;
 }
